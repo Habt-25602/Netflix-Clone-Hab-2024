03e5ea86d41741579297c3012fcde196370dd130
Habtamu Tilahun <habt256@gmail.com>
2024-11-30 00:35:43 +0000

    Vite Configuration

--- Netflix-clone-project/vite.config.js
+++ Netflix-clone-project/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Netflix-Clone-Hab-2024/", // Use your actual repository name
   plugins: [react()],
-})
+});
